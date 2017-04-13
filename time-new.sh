e9d3152d78375c4d119116ab3247510de8ca87e1
Leo Chu <chul@vmware.com>
2017-04-12 21:27:11 -0700

    add some strange things

---
+++ time-new.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+source /root/*lbaas*.sh
+now1="$(date +'%Y%m%d%H%M%S')"
+ILE='EXP_PM_VLB_'$now1.txt
+for lb in $(nova list | egrep -o '172.20.10[4-7].[0-9]{1,3}'); do cat tip.sh|sshpass -p 'CMCC123!@#' ssh -o StrictHostKeyChecking=no -p 2222 root@$lb; done 1>test.txt 
+now2="$(date +'%Y%m%d %H:%M:%S')" ;sed -i "s/20[0-9][0-9][^;]*/$now2/1" test.txt 
+grep '20[1-2][0-9]' test.txt | sort | uniq  >> $FILE 
+HOST='172.20.41.3'
+USER='ftpuser'
+PASSWD='e#Vg_1B7ln'
+echo $FILE
+yum -y install yum
+ftp -n $HOST <<END_SCRIPT
+quote USER $USER
+quote PASS $PASSWD
+cd /var/lib/cmagent/loadbalancer/input/
+put $FILE
+quit
+END_SCRIPT
+#rm -rf $FILE
+exit 0
+
